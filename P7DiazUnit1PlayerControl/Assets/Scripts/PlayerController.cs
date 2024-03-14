7608bdd1a55048b58eba60b6cf6438b3d7d9d3b7
Otrebor0417 <roborobert185@gmail.com>
2024-03-13 18:44:52 -0700

    Made new scripts and added new prefabs
    Made new vehicles and made a camera switcher

--- P7DiazUnit1PlayerControl/Assets/Scripts/PlayerController.cs
+++ P7DiazUnit1PlayerControl/Assets/Scripts/PlayerController.cs
@@ -8,10 +8,13 @@ public class PlayerController : MonoBehaviour
     public float turnSpeed;
     public float horizontalInput;
     public float fowardInput;
+    public Camera mainCamera;
+    public Camera hoodCamera;
+    public KeyCode switchKey;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -23,5 +26,10 @@ public class PlayerController : MonoBehaviour
         transform.Translate(Vector3.forward * Time.deltaTime * speed * fowardInput);
         transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
 
+        if (Input.GetKeyDown(switchKey))
+        {
+            mainCamera.enabled = !mainCamera.enabled;
+            hoodCamera.enabled = !hoodCamera.enabled;
+        }
     }
 }
